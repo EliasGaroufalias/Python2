6472a6e0b0f31bfaf4bfe36da76e8eb71e8715de
EliasGaroufalias <eliassailafourag@gmail.com>
2018-02-21 17:54:31 +0200

    Ex 6 done. Most likely.

--- ask_5.py
+++ ask_5.py
@@ -13,10 +13,15 @@ auth.set_access_token(access_token, access_secret)
 api = tweepy.API(auth)
 
 tweets=''
-username = raw_input("\nGive desired user's username:\n\n\t")
-for status in tweepy.Cursor(api.user_timeline, screen_name=username, tweet_mode = 'extended').items(10):
-	tweets += status._json['full_text']
-	tweets += ' '
+while True:
+	try:
+		username = raw_input("\nGive desired user's username:\n\n\t")
+		for status in tweepy.Cursor(api.user_timeline, screen_name=username, tweet_mode = 'extended').items(10):
+			tweets += status._json['full_text']
+			tweets += ' '
+		break
+	except:
+		print '\nWrong Input, try again\n\n\n'
 tweets = tweets.split()
 for i in range(len(tweets)):
 	tweets[i] = tweets[i].lower()
@@ -34,6 +39,6 @@ for i in range(1,len(c)):
 	if c[i]>maxx:
 		maxx=c[i]
 		maxi=i
-print '\n\nH pio dhmofilhs le3h apo ton', username, "einai '" + tweets[maxi] + "'"
+print "\n\nThe most popular word from user '" + username + "' einai '" + tweets[maxi] + "'"
 
 #todo: input checks and co.
