617e3db84165dbbc67b8483a4623c8e175c8b13e
EliasGaroufalias <36332749+EliasGaroufalias@users.noreply.github.com>
2018-02-22 16:47:03 +0200

    Now works on linux as well. And now supports loops in loops

--- ask_9.py
+++ ask_9.py
@@ -1,61 +1,56 @@
-from __future__ import print_function
-import os
+from _future_ import print_function
 
-source_path=raw_input('\n\nGive the name of the brainfuck code file:\n\n\t')
-source_path1=list(source_path)
-if source_path1[len(source_path1)-4]!='.' or source_path1[len(source_path1)-3]!='t' or source_path1[len(source_path1)-2]!='x' or source_path1[len(source_path1)-1]!='t':
-	source_path=source_path+'.txt'
-source_path2='./'+source_path
-
-if os.path.isfile(source_path2):
-	print('\n\nProgram is being executed.\n\n\tInput-Output:\n\n\n')
-	input_file=open(source_path,'r')
-	source_code=list(input_file.read())
-	l=len(source_code)
-	registry1=[0]*30000
-	i=0
-	j=0
-	while j<l:
-		if source_code[j]=='>':
-			if i<29999:
-				i+=1
-			else:
-				print("\nError 1: Registry index out of range.")
-		elif source_code[j]=='<':
-			if i>0:
-				i-=1
-			else:
-				print("\nError 1: Registry index out of range.")
-				break
-		elif source_code[j]=='+':
-			if registry1[i]<255:
-				registry1[i]+=1
-			else:
-				print("\nError 2: Registry value overflow.")
-				break
-		elif source_code[j]=='-':
-			if registry1[i]>0:
-				registry1[i]-=1
-			else:
-				print("\nError 3: Registry value can't be negative.")
-				break
-		elif source_code[j]==',':
-			#print('make input pretty? no?')
-			try:
-				registry1[i]=ord(raw_input())
-			except:
-				print("\nError 4: Invalid value.")
-				break
-		elif source_code[j]=='.':
-			print(chr(registry1[i]),end='')
-		elif source_code[j]=='[':
-			loop_start=j
-		elif source_code[j]==']':
-			if registry1[i]!=0:
-				j=loop_start
-		j+=1
-	print('\n\n\n')
-else:
-	print('\n\nThere is no such file\n', end='\n')
-
-#to do: a proper compiler must have custom error messages, so should this translator.
+source_code=list(raw_input('\n\nGive the brainfuck code:\n\n\t'))
+print('\n\nProgram is being executed.\n\n\tInput-Output:\n\n\n')
+l=len(source_code)
+registry1=[0]*30000
+i=0
+j=0
+k=0
+loop_start=[]
+while j<l:
+	if source_code[j]=='>':
+		if i<29999:
+			i+=1
+		else:
+			print("\nError 1: Registry index out of range.")
+	elif source_code[j]=='<':
+		if i>0:
+			i-=1
+		else:
+			print("\nError 1: Registry index out of range.")
+			break
+	elif source_code[j]=='+':
+		if registry1[i]<255:
+			registry1[i]+=1
+		else:
+			print("\nError 2: Registry value overflow.")
+			break
+	elif source_code[j]=='-':
+		if registry1[i]>0:
+			registry1[i]-=1
+		else:
+			print("\nError 3: Registry value can't be negative.")
+			break
+	elif source_code[j]==',':
+		#print('make input pretty? no?')
+		try:
+			registry1[i]=ord(raw_input())
+		except:
+			print("\nError 4: Invalid value.")
+			break
+	elif source_code[j]=='.':
+		print(chr(registry1[i]),end='')
+	elif source_code[j]=='[':
+		k+=1
+		if len(loop_start)<k:
+			loop_start.append(j)
+		else:
+			loop_start[k-1] = j
+	elif source_code[j]==']':
+		if registry1[i]!=0:
+			j=loop_start[k-1]
+		else:
+			k-=1
+	j+=1
+print('\n\n\n')
